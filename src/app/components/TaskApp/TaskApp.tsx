2c5a7e20df1d206f346797c688d1abb4bb5e8680
Pedro <pedroasso@hotmail.com>
2025-01-22 22:26:20 -0500

    [design]: add tailwind style

--- src/app/components/TaskApp/TaskApp.tsx
+++ src/app/components/TaskApp/TaskApp.tsx
@@ -1,44 +1,71 @@
-'use client';
+"use client";
 
 import { useState } from "react";
+import Image from "next/image";
+import BG from "../../public/bg.jpg";
 
 export const TaskApp = () => {
-    const [task, setTask] = useState('');
-    const [taskList, setTaskList] = useState([]);
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
 
-    const getTask = (e) => {
-        setTask(e.target.value);
+  const getTask = (e) => {
+    setTask(e.target.value);
+  };
+
+  const inputSubmit = (e) => {
+    e.preventDefault();
+    if (task.trim()) {
+      setTaskList([...taskList, task]);
+      setTask("");
     }
+  };
 
-    const inputSubmit = (e) => {
-        e.preventDefault();
-        if (task.trim()) {
-            setTaskList([...taskList, task]);
-            setTask('')
-        }
-    }
+  const deleteTask = (index) => {
+    setTaskList(taskList.filter((_, i) => i !== index));
+  };
 
-    const deleteTask = (index) => {
-        setTaskList(taskList.filter((_, i) => i !== index));
-    }
-
-    return (
-        <div className="w-full text-center mt-12">
-            <div className="">
-                <h1 className="text-2xl">Add a Task</h1>
-                <form onSubmit={inputSubmit} className="">
-                    <input className="p-2 m-4 border-solid border-black border-2 rounded-md" type="text" value={task} onChange={getTask} placeholder="Add your task" />
-                    <button className="p-2 m-4 border-solid border-black border-2 rounded-md bg-blue-500" type="submit">Add Task</button>
-                </form>
-                <ul>
-                    {taskList.map((task, index) => (
-                        <li key={index}>
-                            {task}
-                            <button onClick={()=> deleteTask(index)}>Delete Task</button>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+  return (
+    <div
+      className="w-screen h-screen bg-contain bg-center flex flex-col justify-center items-center text-center"
+      style={{ backgroundImage: `url(${BG.src})` }}
+    >
+      <h1 className="text-4xl font-bold pb-8">My To-Do-List</h1>
+      <div className="relative w-8/12 h-4/5 p-4">
+        <div className="absolute inset-0 bg-yellow-200 opacity-50 rounded-lg"></div>
+        <div className="relative z-10">
+          <form onSubmit={inputSubmit} className="">
+            <input
+              className="w-10/12 p-2 m-4 border-solid border-black border-2 rounded-md"
+              type="text"
+              value={task}
+              onChange={getTask}
+              placeholder="Add your task"
+            />
+            <button
+              className="p-2 m-4 border-solid border-black border-2 rounded-md bg-blue-500"
+              type="submit"
+            >
+              Add Task
+            </button>
+          </form>
+          <ul className="flex flex-wrap flex-col content-center mt-4">
+            {taskList.map((task, index) => (
+              <li
+                key={index}
+                className="w-10/12 h-12 px-2 bg-white flex flex-wrap items-center justify-between mb-2 border-solid border-black border-2 rounded-md"
+              >
+                {task}
+                <button
+                  className="p-1 bg-red-600 justify-items-end border-solid border-2 border-black rounded-md"
+                  onClick={() => deleteTask(index)}
+                >
+                  Delete
+                </button>
+              </li>
+            ))}
+          </ul>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import { TaskApp, Header } from "./components";
+import { TaskApp } from "./components";
 
 export default function Home() {
   return (
-    <div className="w-full m-0 p-0">
-      <Header />
+    <div className="overflow-hidden">
       <TaskApp />
     </div>
   );
